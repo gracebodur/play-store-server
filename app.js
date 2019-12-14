838dd616da841a0ba4ca8f15fd5498436a6fcb9d
Alvin <alvinpl21@gmail.com>
2019-12-14 13:30:05 -0500

    fixing sort query

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 const express = require ('express');
 const morgan = require('morgan');
-const data = require('./playstoreData.js')
+const datas = require('./playstoreData.js')
 
 const app = express()
 
 app.use(morgan('common'))
 
-app.get('/apps',(res, req) => {
-    console.log('apps');
+app.get('/apps',(req, res) => {
+    // res.json(data)
+    const { sort = ''} = req.query
+    // const App = data.sort(App)
+
+    if(sort) {
+        if(!['Rating', 'App'].includes(sort)) {
+            return res  
+                .status(400)
+                .send('Please add Rating or App as a sorting type')
+        }
+    }
+
+    let results = datas.filter(data =>
+        {
+        return data.App.toLowerCase().includes(data.toLowerCase())
+        }
+    )
+            // app
+            // .toLowerCase()
+            // .includes(app.toLowerCase()))
+
+    if(sort === 'App') {
+        results.sort((a,b) => {
+            return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+        })
+    }
+
+ res.json(results)
 })
 
 
