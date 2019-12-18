adc9dbaf071841bfc7788d0fe4d105ea6fc7630d
Grace <mgracebodur@gmail.com>
2019-12-17 19:04:54 -0500

    Preparing app for testing

--- app.js
+++ app.js
@@ -65,8 +65,9 @@ app.get('/apps',(req, res) => {
  res.json(results)
 })
 
+module.exports = app
 
-app.listen(8000, () => {
-    console.log('Server started on PORT 8000')
-})
+// app.listen(8000, () => {
+//     console.log('Server started on PORT 8000')
+// })
 
